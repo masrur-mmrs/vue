c915a9fc9e6aeaf835bd5662b6d638e7637ab239
masrur-mmrs <masrur.mmrs@gmail.com>
2021-04-21 00:40:22 +0600

    Vue app lifecycle

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -10,12 +10,42 @@ const app = Vue.createApp({
             this.currentUserInput = event.target.value;
         },
         setText() {
-            this.message = this.currentUserInput;
+            // this.message = this.currentUserInput;
+            this.message = this.$refs.userText.value;
+            // console.dir(this.$refs.userText);
         },
+    },
+    beforeCreate() {
+        console.log("Before Create");
+    },
+    created() {
+        console.log("created");
+    },
+    beforeMount() {
+        console.log("Before Mount");
+    },
+    mounted() {
+        console.log("Mounted");
+    },
+    beforeUpdate() {
+        console.log("Before Update");
+    },
+    updated() {
+        console.log("Updated");
+    },
+    beforeUnmount() {
+        console.log("Before unmount");
+    },
+    unmounted() {
+        console.log("Unmounted");
     },
 });
 
 app.mount("#app");
+
+setTimeout(() => {
+    app.unmount();
+}, 3000);
 
 const app2 = Vue.createApp({
     template: `<p>{{favouriteMeal}}</p>`,
